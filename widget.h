8dcdb289aaad38832102124a72daf9866d39ece3
user <user@DESKTOP-LI69LEU>
2023-09-24 19:57:35 +0300

    init

---
+++ widget.h
@@ -0,0 +1,43 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QMediaPlayer>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Widget; }
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private slots:
+    void on_horizontalSliderVolume_valueChanged(int value);
+
+    void on_pushButtonOpen_clicked();
+
+    void on_pushButtonPlay_clicked();
+
+    void on_pushButtonPause_clicked();
+
+    void on_pushButtonStop_clicked();
+
+    void on_position_changed(qint64 value);
+    void on_duration_changed(qint64 position);
+
+
+    void on_horizontalSliderTrak_valueChanged(qint64 position);
+
+    void on_horizontalSliderTrak_sliderMoved(int position);
+
+private:
+    Ui::Widget *ui;
+
+    QMediaPlayer* m_player;
+};
+#endif // WIDGET_H
